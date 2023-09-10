3ca73733f8cb2f074903d28efb7e42c65a4dbc6c
Tristan Barnhouse <75106838+TDBarnhouse@users.noreply.github.com>
2023-09-10 02:50:55 -0400

    Updated bot

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Mini-DJ-Beastie
 Personal music bot for Beastie's discord server.
+
+Credit to @eritislami for the base of the bot.
--- variables/logos.js
+++ variables/logos.js
@@ -1,10 +1,3 @@
-const siegeLogo = '<:siegelogo:483324491984797697>';
-const discordLogo = '<:discord:397641882248675328>';
-const beastieLogo = '<:beastielogo:397646118168821761>';
-const steamLogo = '<:steam:397643865454280714>';
-const twitchLogo = '<:twitch:397641913186123776>';
-const twitterLogo = '<:twitter:397618689106640896>';
-const youtubeLogo = '<:youtube:397642669796622338>';
 const greenCheck = '<:greenCheck:1094704353526436050>';
 const redX = '<:redX:1094727493111513168>';
 
